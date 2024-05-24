3830903f08bad5cce5a461b85c5b020be1b8f2b2
Thiago Melo <thiago@fanbase.com.br>
2024-05-24 17:05:02 -0300

    feat: Add data_4

---
+++ Projetos/FUNDAMENTOS/Inicio/data_4.js
@@ -0,0 +1,13 @@
+// Trabalhando com Dados #4
+// Modificar / Subistituir o valor da variavel
+
+// const(constante) é um valor / variavel que não pode ser alterado(a)
+const a = 3;
+
+// ...
+
+// a = a + 10;
+
+// ...
+
+console.log(a);
