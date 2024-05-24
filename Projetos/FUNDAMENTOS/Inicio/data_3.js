3a85b9f6540496e0996a460376249c883507a3e7
Thiago Melo <thiago@fanbase.com.br>
2024-05-24 16:52:35 -0300

    feat: Add data_3

---
+++ Projetos/FUNDAMENTOS/Inicio/data_3.js
@@ -0,0 +1,16 @@
+// Trabalhando com Dados #3
+
+console.log("Tipos básicos:");
+
+let age = 31;
+console.log(typeof 31);
+console.log(typeof age);
+
+let wage = 4000.00;
+console.log(typeof wage);
+
+let itsRaining = true // ou false;
+console.log(typeof itsRaining);
+
+console.log(typeof "Test");
+console.log(typeof 'Test');
