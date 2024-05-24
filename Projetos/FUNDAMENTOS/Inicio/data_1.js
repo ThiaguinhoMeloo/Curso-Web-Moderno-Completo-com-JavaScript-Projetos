1ffd85eeba947b8a4c408715a31549653b408612
Thiago Melo <thiago@fanbase.com.br>
2024-05-24 16:05:21 -0300

    feat: Add data_1

---
+++ Projetos/FUNDAMENTOS/Inicio/data_1.js
@@ -0,0 +1,10 @@
+var qtdCanetas = 10;
+var price = 6.4;
+let tax = 1.5;
+let finalPrice = price + tax;
+
+console.log("Comprando Canetas:");
+console.log("Quantidade de Canetas: " + qtdCanetas + " unidades.");
+console.log("Preço Unitário: " + price + "$");
+console.log("Imposto: " + tax + "$");
+console.log("Preço final: " + finalPrice + "$");
